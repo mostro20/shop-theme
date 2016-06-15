859cc3154d80675e65e6b42c696575a1619d96df
Morgan Strong <mostro20@gmail.com>
2016-06-15 16:02:45 +1000

    Magento theme update
    This will be the base of the Magento theme going forward. There are
    still a few aspects with hard-coded Yume variants that need to be
    addressed, and also the theme will be updated with the header sections
    developed for the web and research front-end.

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Gaia/rwahstweaks',
+    'frontend/Gaia/rwahs',
     __DIR__
 );
